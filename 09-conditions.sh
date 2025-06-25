a3e89cfbc0ec08573337c264a39333aea49f8c3d
rajesh <mrajesh0525@gmail.com>
2025-06-25 16:21:08 +0530

    conditions

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -4,7 +4,7 @@ Num=$1
 
 if [ $Num -gt 10 ]
 then
-    echo "Given number $Num is greater than 10"
+    echo "Given number $Num is less than 10"
 
 else
     echo "Given number $Num is not less than 10"
