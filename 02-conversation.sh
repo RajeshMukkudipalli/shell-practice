0d14cb5d5ba64ea34724a1727cdbb857901771ae
rajesh <mrajesh0525@gmail.com>
2025-07-14 23:56:26 +0530

    shell-practice

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
-echo "Trump: Hey musk, How are you"
-echo "Musk: Hey Trump, I am good. How are you?"
+person1="Trump"
+person2="Musk"
+echo "$person1: Hey musk, How are you"
+echo "$person2: Hey Trump, I am good. How are you?"
 echo "Trump: I am good too. What are you doing these days?"
 echo "Musk: I am working on my new project. What about you?"
 echo "Trump: I am working on my new project too. What is your project about?"
