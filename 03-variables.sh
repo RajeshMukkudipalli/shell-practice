77e55b07fc6150ba30fd7d14cb5a9d4b8bfc24c7
rajesh <mrajesh0525@gmail.com>
2025-07-15 00:21:08 +0530

    shell-practice

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+person1="Trump"
+person2="Musk"
+echo "$person1: Hey $person2, How are you"
+echo "$person2: Hey $person1, I am good. How are you?"
+echo "$person1: I am good too. What are you doing these days?"
+echo "$person2: I am working on my new project. What about you?"
+echo "$person1: I am working on my new project too. What is your project about?"
+echo "$person2: My project is about space travel. What about you?"
+echo "$person1: My project is about making America great again."
