d49e2b111b6c4d314191b67eb7a2de2501b517ad
rajesh <mrajesh0525@gmail.com>
2025-07-14 21:31:50 +0530

    shell-practice

---
+++ 10-install-commands.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+USERID=$(id -u)
+if [ $USERID -ne 0 ]
+ then
+    echo "you are not root user. Please run this script as root user"
+    exit 1 
+else
+   echo "You are root user. Proceeding with installation commands"
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0]
+then
+    echo "MySQL is not installed...proceeding with installation"
+else
+    echo "mySQL is already installed...nothing to do"
+    exit 1
+fi
+    
+dnf install mysql -y
+
+if [ $? -eq 0]
+then
+    echo "MySQL istallation is successful"
+else
+    echo "'MYSQL Installation failed"
+    exit 1
+fi
