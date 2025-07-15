76827e51997d7a98dec7ee7380418e3d98d2efb2
rajesh <mrajesh0525@gmail.com>
2025-07-15 21:57:23 +0530

    shell-practice

---
+++ 11-functions.sh
@@ -0,0 +1,52 @@
+#! /bin/bash
+
+userid=$(id -u)
+
+if [ $userid -ne 0 ]; then
+    echo "You are root user"
+else
+    echo "You are not root user"
+fi
+
+validate() {
+    if [ $1 -eq 0 ]; then
+        echo "Installing $2 is successful"
+    else
+        echo "Installing $2 is failed"
+        exit 1
+    fi
+}
+
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+    echo "MySQL is not installed...proceeding with installation"
+    dnf install mysql -y
+    validate $? "MySQL"
+else
+    echo "mySQL is already installed...nothing to do"
+fi
+
+dnf list installed python3
+
+if [ $? -ne 0 ]
+then
+    echo "MySQL is not installed...proceeding with installation"
+    dnf install python3 -y
+    validate $? "python3"
+else
+    echo "python3 is already installed...nothing to do"
+fi
+
+dnf list installed nginx
+
+if [ $? -ne 0 ]
+then
+    echo "MySQL is not installed...proceeding with installation"
+    dnf install nginx -y
+    validate $? "nginx"
+else
+    echo "nginx is already installed...nothing to do"
+fi
