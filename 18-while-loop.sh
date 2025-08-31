ef1de72b7cd7425167c363d222cf03bfbd07b273
rajesh <mrajesh0525@gmail.com>
2025-09-01 01:23:03 +0530

    shell-roboshop

--- 18-while-loop.sh
+++ 18-while-loop.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-a=0
+# a=0
 
-while [ $a -lt 10 ]
+# while [ $a -lt 10 ]
+# do
+#     echo $a
+#     a='expr $a + 1'
+# done
+
+while IFS= read -r line
 do
-    echo $a
-    a='expr $a + 1'
-done
+    echo "$line"
+done < 17-set.sh
+# here IFS is internal field separator which is used to split the line into words and -r is used to prevent backslash from being interpreted as escape character
+# < 17-set.sh is used to read the file line by line
+# here while loop will read the file line by line and print each line
+# IFS and -r are optional but it's a good practice to use them
