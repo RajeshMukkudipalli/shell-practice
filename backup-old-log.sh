dbe7e0042600d18f48479d270c5dde5cc13b9127
rajesh <mrajesh0525@gmail.com>
2025-09-02 00:50:49 +0530

    shell-roboshop

--- backup-old-log.sh
+++ backup-old-log.sh
@@ -1,78 +1,81 @@
 #!/bin/bash
 
-source_dir =$1
-backup_dir =$2
-days=$(3:-14)
+source_dir=$1
+backup_dir=$2
+days=${3:-14}
+
 source_dir=/home/ec2-user/app-log
 log_folder=/home/ec2-user/old-logs
-script_name=$(echo $0 | cut  -d "." -f1)
+script_name=$(basename "$0" | cut -d "." -f1)
 log_file="$log_folder/$script_name-$(date +'%d-%m-%Y').log"
+
 userid=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-mkdir -p $log_folder
 
-if [ $userid -ne 0 ] 
-then
-    echo "You are not root user"
+mkdir -p "$log_folder"
+
+# Check user
+if [ "$userid" -ne 0 ]; then
+    echo -e "${R}You are not root user${N}"
 else
-    echo "You are  root user"
+    echo -e "${G}You are root user${N}"
 fi
 
 validate() {
     if [ $1 -eq 0 ]; then
-        echo -e "$G Installing $2 is successful"
+        echo -e "$G Installing $2 is successful $N"
     else
-        echo -e "$R Installing $2 is failed"
+        echo -e "$R Installing $2 failed $N"
         exit 1
     fi
 }
 
-usage(){
-
-    echo -e "$R sh back-up.sh <source_directory> <backup_directory> <days>$N"
+usage() {
+    echo -e "$R Usage: sh back-up.sh <source_directory> <backup_directory> <days>$N"
 }
 
-if [ ! -d $source_dir ]; then
-    echo -e "$R Source directory $source_dir does not exist $N"
-    usage
-    exit 1
-fi
-
-if [ ! -d $backup_dir ]; then
-    echo -e "$R Backup directory $backup_dir does not exist $N"
-    usage
-    exit 1
-fi
-
+# Validate arguments
 if [ $# -lt 2 ]; then
     usage
     exit 1
 fi
 
+# Validate directories
+if [ ! -d "$source_dir" ]; then
+    echo -e "$R Source directory $source_dir does not exist $N"
+    usage
+    exit 1
+fi
 
+if [ ! -d "$backup_dir" ]; then
+    echo -e "$R Backup directory $backup_dir does not exist $N"
+    usage
+    exit 1
+fi
 
-File=$(find $source_dir -name "*.log" -mtime +$days)
+# Find old log files
+File=$(find "$source_dir" -name "*.log" -mtime +"$days")
 
-if [ ! -z $File]
-then
-    echo " File found to be zip $File"
+if [ -n "$File" ]; then
+    echo "Files found to be zipped: $File"
     timestamp=$(date +%F-%H-%M-%S)
     Zip_file="$backup_dir/log-backup-$timestamp.zip"
-    echo $File | zip $Zip_file -@
-    if [ -f $Zip_file ]; then
-        echo -e "$G Backup is successful and file is $Zip_file $N"
-        while IFS= read -r file
-        do
+
+    echo "$File" | zip "$Zip_file" -@
+
+    if [ -f "$Zip_file" ]; then
+        echo -e "$G Backup successful: $Zip_file $N"
+        while IFS= read -r file; do
             echo "Deleting file: $file"
-            rm -rf "$file"
+            rm -f "$file"
         done <<< "$File"
-        echo -e "log files lolder than $days days are deleted from $source_dir....."$G SUCCESS $N"
+        echo -e "Log files older than $days days deleted from $source_dir... ${G}SUCCESS${N}"
     else
-        echo -e "$R Backup is failed $N"
+        echo -e "$R Backup failed $N"
     fi
 else
-    echo -e "No log Files found older than 14 days.... $Y Skipping backup $N"
+    echo -e "No log files older than $days days... ${Y}Skipping backup${N}"
 fi
